76f3aa5ea302cf21921d3f83cd22746d139175a6
Stephen Ou <me@stephenou.com>
2015-02-21 16:17:45 -0800

    Added new task functionality and new dashboard

--- hunt/app/controllers/scavenger_controller.rb
+++ hunt/app/controllers/scavenger_controller.rb
@@ -10,6 +10,16 @@ def new
 	end
 end
 
+def dashboard
+	if !session[:login]
+		flash[:notice] = "You need to be logged in to create a scavenger hunt!"
+		redirect_to(:controller => :user, :action => :login)
+	else
+		@scavenger = Scavenger.find(param[:id])
+		@title = @scavenger.name
+	end
+end
+
 def create
 	@scavenger = Scavenger.new(scavenger_params(params[:scavenger]))
 	@scavenger.date_time = DateTime.now
@@ -23,9 +33,33 @@ def create
 	end
 end
 
+def new_task
+	if !session[:login]
+		flash[:notice] = "You need to be logged in to create a scavenger hunt!"
+		redirect_to(:controller => :user, :action => :login)
+	else
+		@task = Task.new
+		@title = "Create a task"
+	end
+end
+
+def create_task
+	@task = Task.new(task_params(params[:task]))
+	if @task.save
+		flash[:notice] = "Task created successfully"
+		redirect_to(:controller => :scavenger, :action => :dashboard, :id => @task.scavenger.id)
+	else
+		flash[:notice] = 'Task not successful'
+		render(:action => :new_task)
+	end
+end
+
 private
 	def scavenger_params(params)
 		return params.permit(:hunt_name, :location, :description)
+	end
+	def task_params(params)
+		return params.permit(:task_name, :point)
 	end
 
 end
